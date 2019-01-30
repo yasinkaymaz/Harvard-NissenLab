945921209c8f19cf5890f1d7c67412bf8a5ae627
Yasin Kaymaz <yasinkaymaz@github.com>
2019-01-30 15:41:50 -0500

    reorganized the results

--- code/WP_codes.R
+++ code/WP_codes.R
@@ -11,13 +11,13 @@ Tasic2016 <- SeuratWrapper(ExpData = exp, ProjectLabel = "Tasic2016", Normalize 
 
 
 #ALM data only
-#cd data
+#cd ~/data
 #wget http://celltypes.brain-map.org/api/v2/well_known_file_download/694413179
 #unzip mouse_ALM_gene_expression_matrices_2018-06-14.zip
 here::here()
-exp <- read.csv(here::here("data/mouse_ALM_2018-06-14_exon-matrix.csv"),row.names = 1,header = T)
-meta <- read.csv(here::here("data/mouse_ALM_2018-06-14_samples-columns.csv"), row.names = 1, header = T)
-geneinfo <- read.csv(here::here("data/mouse_ALM_2018-06-14_genes-rows.csv"), row.names = 4, header = T)
+exp <- read.csv("~/data/mouse_ALM_2018-06-14_exon-matrix.csv",row.names = 1,header = T)
+meta <- read.csv("~/data/mouse_ALM_2018-06-14_samples-columns.csv", row.names = 1, header = T)
+geneinfo <- read.csv("~/data/mouse_ALM_2018-06-14_genes-rows.csv", row.names = 4, header = T)
 
 identical(rownames(exp), rownames(geneinfo))
 rownames(exp) <- geneinfo$gene_symbol
@@ -44,9 +44,13 @@ clusters_of_interest <- c("L5 IT ALM Cbln4 Fezf2",
                           "L5 PT ALM Slco2a1")
 cells <- rownames(Tasic2018@meta.data[which(Tasic2018@meta.data$cluster %in% clusters_of_interest),])
 L5ALM.ITPT <- SubsetData(object = Tasic2018, cells.use = cells,do.center = T,do.scale = T,do.clean = T)
-L5ALM.ITPT <- QuickSeurat(L5ALM.ITPT)
 
-pdf("clusters.pdf",width = 20,height = 10)
+latentVars <- c("nUMI","nGene","percent_ecoli_reads","percent_synth_reads","percent_mt_exon_reads",
+                "percent_rrna_reads")
+
+L5ALM.ITPT <- QuickSeurat(L5ALM.ITPT, vars2reg = latentVars)
+
+pdf("clusters.pdf",width = 10,height = 5)
 TSNEPlot(L5ALM.ITPT, group.by="brain_subregion")
 TSNEPlot(L5ALM.ITPT, group.by="subclass")
 TSNEPlot(L5ALM.ITPT, group.by="class")
@@ -60,22 +64,7 @@ TSNEPlot(L5ALM.ITPT, group.by="age_days")
 TSNEPlot(L5ALM.ITPT, group.by="brain_hemisphere")
 
 L5ALM.ITPT = SetAllIdent(L5ALM.ITPT, id = 'subclass')
-allmarkers.subclass = FindAllMarkers(object = L5ALM.ITPT, logfc.threshold = 0.1,min.pct = 0.05,only.pos = T)
+allmarkers.subclass <- FindAllMarkers(object = L5ALM.ITPT, test.use = "MAST", latent.vars = latentVars, logfc.threshold = 0.1,min.pct = 0.05,only.pos = T)
 
-head(allmarkers.subclass)
-library(plotly)
-p <- ggplot(allmarkers.subclass, aes(avg_logFC, y=-1*log10(p_val_adj), color=cluster ))+
-  geom_point(aes(text=sprintf("Gene: %s", gene)))+
-  ylab("-log10(padj)")+
-  xlab("log Fold Change")
-ggplotly(p)
-
-
-allmarkers.subclass  %>% select(-p_val)%>% filter(cluster == "L5 IT") %>% filter(p_val_adj < 0.01) %>%
-  datatable( extensions = 'Scroller', options = list(
-    deferRender = TRUE,
-    scrollY = 200,
-    scroller = TRUE
-  ))
 save(Tasic2018, L5ALM.ITPT, allmarkers.subclass, file=here::here("data/Tasic2018-reAnalysis.RData"))
 
